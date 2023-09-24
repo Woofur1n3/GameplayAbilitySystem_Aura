ee9e014efce9f49c0aad462e9d873cbb8da609ce
Woofurine <woofur1n3@gmail.com>
2023-09-24 16:43:29 +0800

    2_6.Player and Enemy Characters

---
+++ Source/Aura/Private/Character/AuraCharacterBase.cpp
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Character/AuraCharacterBase.h"
+
+AAuraCharacterBase::AAuraCharacterBase()
+{
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = false;
+
+}
+
+// Called when the game starts or when spawned
+void AAuraCharacterBase::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
