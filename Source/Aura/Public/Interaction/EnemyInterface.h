e1f42be0c846ce3488d71fade8e804f16c7c0183
Woofurine <woofur1n3@gmail.com>
2023-09-30 10:42:41 +0800

    2_13.Enemy Interface

---
+++ Source/Aura/Public/Interaction/EnemyInterface.h
@@ -0,0 +1,29 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Interface.h"
+#include "EnemyInterface.generated.h"
+
+// This class does not need to be modified.
+UINTERFACE(MinimalAPI)
+class UEnemyInterface : public UInterface
+{
+	GENERATED_BODY()
+};
+
+/**
+ * 
+ */
+class AURA_API IEnemyInterface
+{
+	GENERATED_BODY()
+
+	// Add interface functions to this class. This is the class that will be inherited to implement this interface.
+public:
+	//Pure virtual function.
+	//Any class that implement this interface required to override this function.
+	virtual void HighlightActor() = 0;
+	virtual void UnHighlightActor() = 0;
+};
